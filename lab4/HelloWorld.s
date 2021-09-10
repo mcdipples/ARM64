585a5ebd8998ca2ffb3647e0aefea236e098a54c
Sierra Martin <86510695+mcdipples@users.noreply.github.com>
2021-09-09 23:17:38 -0700

    Update HelloWorld.s

--- lab4/HelloWorld.s
+++ lab4/HelloWorld.s
@@ -15,17 +15,17 @@ _start:
  svc 0 		 // call linux to output the string
 
 // Print newline '\n' char.. this is ascii 10
-// mov X0, #1	 // 1 = StdOut
+ mov X0, #1	 // 1 = StdOut
  ldr X1, =chCr   // char to print
  mov X2, #1 	 // length of char (1 byte)
-// mov X8, #64
+ mov X8, #64
  svc 0		 // output the string again
 
 // Print second string
-// mov X0, #1
+ mov X0, #1
  ldr X1, =szMsg2 // string to print
  mov X2, #24     // length of our sting (plus null char)
- //mov X8, #64 // Linux write system call
+ mov X8, #64 // Linux write system call
  svc 0 // Call Linux to output the string
 
 // Setup the parameters to exit the program
@@ -33,6 +33,7 @@ _start:
  mov X0, #0 // Use 0 return code
  mov X8, #93 // Service code 93 terminates
  svc 0 // Call Linux to terminate
+ 
 .data
 szMsg1: .asciz "The sun did not shine."
 szMsg2: .asciz "It was too wet to play."
